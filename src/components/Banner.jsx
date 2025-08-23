7a7c9eaf40e61cadfe9704432a625399f94e9cee
= <=>
2025-08-23 21:06:41 +0530

    seed updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -6,31 +7,32 @@ import "swiper/css/effect-fade";
 
 const slides = [
   {
-    subtitle: "Making a Difference",
-    title1: "Changing Lives, One",
-    title2: "Donation at a Time",
-    text: "Explore the variety of volunteer opportunities available...",
-    bgImage: "/assets/img/hero/hero_bg_4_1.jpg",
+    subtitle: "Empowering Communities",
+    title1: "Together for a Better",
+    title2: "Social & Educational Future",
+    text: "Join SEED in creating opportunities for education, empowerment, and sustainable development.",
+    bgImage: "/uploads/banner.jpg",
   },
   {
-    subtitle: "Give Hope For Homeless",
-    title1: "Helping Each Other",
-    title2: "Make World Better",
-    text: "Explore the variety of volunteer opportunities available...",
-    bgImage: "/assets/img/hero/hero_bg_4_2.jpg",
+    subtitle: "Building a Greener Tomorrow",
+    title1: "Environmental Care",
+    title2: "Starts with Us",
+    text: "Be a part of SEED's initiatives to protect nature and promote a cleaner, healthier planet.",
+    bgImage: "uploads/banner2.jpg",
   },
   {
-    subtitle: "Making a Difference",
-    title1: "Every Donation Counts",
-    title2: "Help Us Change Lives",
-    text: "Explore the variety of volunteer opportunities available...",
-    bgImage: "/assets/img/hero/hero_bg_4_3.jpg",
+    subtitle: "Your Support Matters",
+    title1: "Education, Equality,",
+    title2: "and Empowerment",
+    text: "Contribute to SEED’s mission to uplift lives through education, social change, and environmental action.",
+    bgImage: "uploads/banner3.jpg",
   },
 ];
 
+
 const Banner = () => {
   return (
-    <div className="th-hero-wrapper hero-4" id="hero">
+    <div className="th-hero-wrapper hero-4" id="hero" style={{ height: "600px" }}>
       <Swiper
         effect="fade"
         loop={true}
@@ -54,20 +56,20 @@ const Banner = () => {
                     <div className="hero-style4 text-white">
                       <span className="sub-title after-none">{slide.subtitle}</span>
                       <h1 className="hero-title text-white">
-                        <span className="title1">{slide.title1}</span>{" "}
-                        <span className="title1">{slide.title2}</span>
+                        <span id="bannertitle1" className="title1">{slide.title1}</span>{" "}
+                        <span id="bannertitle1" className="title1">{slide.title2}</span>
                       </h1>
                       <p className="hero-text text-white">{slide.text}</p>
                       <div className="btn-wrap">
-                        <a href="about.html" className="th-btn">
-                          Discover Now <i className="fa-solid fa-arrow-up-right ms-2"></i>
-                        </a>
-                        <a
+                       
+
+                        <Link to="/about/overview" className="th-btn">About Us <i className="fa-solid fa-arrow-up-right ms-2"></i></Link>
+                        {/* <a
                           href="https://www.youtube.com/watch?v=_sI_Ps7JSEk"
                           className="play-btn style3 popup-video"
                         >
                           <i className="fas fa-play"></i>
-                        </a>
+                        </a> */}
                       </div>
                     </div>
                   </div>
